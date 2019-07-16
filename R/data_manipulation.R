5c7b8284615b7f7da72dd93a0e28c24f89e1003b
Janos Poor <poorjanos@gmail.com>
2019-07-16 15:22:07 +0200

    Add for loop solution for stock counting

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -1,4 +1,67 @@
-# Define helper funcs and create sample -----------------------------------
+library(dplyr)
+
+
+# Generate random sample --------------------------------------------------
+t_cases <- tibble(START = sample(seq(as.Date('2019/01/01'),
+                                     Sys.Date(), by="day"), 10)) %>%
+  # Add random noise to get end dates
+  mutate(DIFF = sample(50, size = nrow(.), replace = TRUE),
+         END = START + DIFF) %>% 
+  # Set end dates greater than Sys.Date to NA
+  mutate(END = replace(END, END >= Sys.Date(), NA)) %>% 
+  select(START, END)
+
+
+# Genarate cutpoints ------------------------------------------------------
+# Full range of days in sample up until Sys.Date
+t_cp_days <-  tibble(CUTPOINTS = seq(min(df$START), Sys.Date(), by = 1))
+# First days of months in sample
+t_cp_months <- tibble(
+  CUTPOINTS = unique(lubridate::floor_date(cp_days$CUTPOINTS, unit = "month"))
+  )
+
+
+# For loop solution -------------------------------------------------------
+get_stock_loop <- function(df, cutpoints) {
+  # Fuction to count number of cases in stock for given cutpoints
+  # 
+  # Args
+  #   df: Data Frame of cases with START and END dates
+  #   cutpoints: Data Frame of cutpoints to compute stock for
+  #   
+  # Returns
+  #   Data Frame: cutpoints extended with col STOCK holding results of
+  #   stock computation for each cutpoint
+  
+  # Define return obj
+  stock <- cutpoints
+  stock$STOCK <- 0
+
+  # Loop on cutpoints
+  for (cutpoint in 1:nrow(cutpoints)) {
+    res <- 0
+    # Loop on cases
+    for (case in 1:nrow(df)) {
+      # Test for stock membership
+      if (cutpoints[[cutpoint, "CUTPOINTS"]] >= df[[case, "START"]] &
+          (is.na(df[[case, "END"]]) | cutpoints[[cutpoint, "CUTPOINTS"]] <= df[[case, "END"]])){
+        res <- res + 1
+      }
+    }
+    # Save computation results to return obj
+    stock[cutpoint, "STOCK"] <- res
+  }
+  return(stock)
+}
+
+
+# Test get_stock_loop
+months <- get_stock_loop(df, cp_months)
+days <- get_stock_loop(df, cp_days)
+
+
+
+# purrr solution ----------------------------------------------------------
 # Define date check function
 check_date <- function(start_date, end_date, check_date) {
   if (check_date >= start_date & (is.na(end_date) | check_date <= end_date)) {
@@ -7,6 +70,26 @@ check_date <- function(start_date, end_date, check_date) {
     return(data.frame("IN_STOCK"=0))
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Test date check function
 s <-  as.Date('2019-01-10')
@@ -22,7 +105,7 @@ seq(as.Date(min(t_stock_head$CREATED)), Sys.Date(), by = 1)
 # Purrr solution ----------------------------------------------------------
 t_stock_head %>%
 mutate(id = as.character(row_number())) %>% 
-left_join(purrr::pmap_dfr(list(t_stock_head$CREATED, t_stock_head$CLOSED), check_date2, as.POSIXct('2018-09-01'), .id = "id"), by = "id") %>% 
+left_join(purrr::pmap_dfr(list(t_stock_head$CREATED, t_stock_head$CLOSED), check_date, as.POSIXct('2018-09-01'), .id = "id"), by = "id") %>% 
 summarize(sum(IN_STOCK))
 
 
