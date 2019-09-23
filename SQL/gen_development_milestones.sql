d72ed44329076965c17a97baa911ffb0d4bdcbe6
Janos Poor <poorjanos@gmail.com>
2019-09-23 14:31:50 +0200

    Add voting time correction

--- SQL/gen_development_milestones.sql
+++ SQL/gen_development_milestones.sql
@@ -1,9 +1,12 @@
 /******************************************************************************/
-/* Generate view of DEVELOPMENT tickets */
-/* Depends on gen_tickets_base.sql */
-/* Includes open and closed tickets */
-/******************************************************************************/
 
+/* Generate view of DEVELOPMENT tickets */
+
+/* Depends on gen_tickets_base.sql */
+
+/* Includes open and closed tickets */
+
+/******************************************************************************/
 DROP TABLE t_dev_milestones;
 COMMIT;
 
@@ -11,12 +14,10 @@ CREATE TABLE t_dev_milestones
 AS
    SELECT   *
      FROM   t_isd_tickets
-    WHERE  REGISTRATIONIDPREFIX IN ('DEV', 'SDEV')
+    WHERE   REGISTRATIONIDPREFIX IN ('DEV', 'SDEV')
             OR CLASSIFICATION = 'Fejlesztési igény (RFC)';
 
 COMMIT;
-
-
 ALTER TABLE t_dev_milestones
 ADD
 (
@@ -25,10 +26,16 @@ release_start date,
 backlog_end date,
 uat_start date,
 uat_end date,
-aborted char(2)
+aborted char(2),
+start_close_days number,
+pre_demand_days number,
+demand_days number,
+release_days number,
+start_close_net_days number,
+demand_net_days number,
+release_net_days number
 );
 COMMIT;
-
 
 UPDATE   t_dev_milestones a
    SET   demand_start =
@@ -37,8 +44,7 @@ UPDATE   t_dev_milestones a
               WHERE   a.oid = b.issue
                       AND REGEXP_LIKE (b.issuestatenew,
                                        '^#02.*|^02.*|^S02.*'));
-    
-                                      
+
 UPDATE   t_dev_milestones a
    SET   backlog_end =
             (SELECT   MIN (modifieddate)
@@ -46,7 +52,6 @@ UPDATE   t_dev_milestones a
               WHERE   a.oid = b.issue
                       AND REGEXP_LIKE (b.issuestatenew,
                                        '^#17.*|^12.*|^S17.*'));
-                                       
 
 UPDATE   t_dev_milestones a
    SET   release_start =
@@ -55,7 +60,7 @@ UPDATE   t_dev_milestones a
               WHERE   a.oid = b.issue
                       AND REGEXP_LIKE (b.issuestatenew,
                                        '^#13.*|^10.*|^S15.*'));
-                                       
+
 UPDATE   t_dev_milestones a
    SET   uat_start =
             (SELECT   MIN (modifieddate)
@@ -64,7 +69,6 @@ UPDATE   t_dev_milestones a
                       AND REGEXP_LIKE (b.issuestatenew,
                                        '^#27.*|^17.*|^S27.*'));
 
-
 UPDATE   t_dev_milestones a
    SET   uat_end =
             (SELECT   MIN (modifieddate)
@@ -72,8 +76,8 @@ UPDATE   t_dev_milestones a
               WHERE   a.oid = b.issue
                       AND REGEXP_LIKE (b.issuestatenew,
                                        '^#28.*|^18.*|^S28.*'));
-COMMIT;
 
+COMMIT;
 
 UPDATE   t_dev_milestones a
    SET   aborted = 'I'
@@ -88,50 +92,283 @@ UPDATE   t_dev_milestones a
 
 COMMIT;
 
+UPDATE   t_dev_milestones a
+   SET   start_close_days = closed - created
+ WHERE   closed IS NOT NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   pre_demand_days = demand_start - created
+ WHERE   created IS NOT NULL AND demand_start IS NOT NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   demand_days = release_start - demand_start
+ WHERE   release_start IS NOT NULL AND demand_start IS NOT NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   release_days = closed - release_start
+ WHERE   release_start IS NOT NULL AND closed IS NOT NULL;
+
+COMMIT;
+
+
+/******************************************************************************/
+
+/* Generate table of pending days  due to voting for development tickets */
+
+/* Depends on t_dev_milestones */
+
+/* Includes open and closed tickets */
+
+/******************************************************************************/
+DROP TABLE t_dev_wait_time;
+COMMIT;
+
+CREATE TABLE t_dev_wait_time
+AS
+     SELECT                       /* Group wait times by phases for tickets */
+           oid,
+              case_id,
+              class_short,
+              wait_phase,
+              SUM (wait_time_day) AS wait_time_day
+       FROM   (SELECT /* Flag waiting times for demand and release phases separately */
+                     oid,
+                        case_id,
+                        class_short,
+                        activity,
+                        step,
+                        timestamp,
+                        lag_timestamp,
+                        wait_time_day,
+                        CASE
+                           WHEN (class_short = 'DEV'
+                                 AND step IN
+                                          ('#03-#04',
+                                           '#05-#06',
+                                           '#07-#08',
+                                           '#09-#10',
+                                           '#12-#13'))
+                                OR (class_short = 'VIP'
+                                    AND step IN ('#05-#06', '#09-#10'))
+                                OR (class_short = 'SDEV'
+                                    AND step IN ('S09-S15'))
+                                OR (class_short = 'RFC' AND step IN ('09-10'))
+                           THEN
+                              'demand'
+                           ELSE
+                              'release'
+                        END
+                           AS wait_phase
+                 FROM   (SELECT /* Generate from-to phases for voting times for different ticket types */
+                               oid,
+                                  case_id,
+                                  class_short,
+                                  activity,
+                                  lag_activity_code || '-' || activity_code
+                                     AS step,
+                                  timestamp,
+                                  lag_timestamp,
+                                  timestamp - lag_timestamp AS wait_time_day
+                           FROM   (  SELECT   tickets.oid,
+                                              tickets.case_id,
+                                              tickets.class_short,
+                                              status.ISSUESTATENEW AS ACTIVITY,
+                                              REGEXP_SUBSTR (
+                                                 status.ISSUESTATENEW,
+                                                 '[#S]*\d{2}\w*'
+                                              )
+                                                 AS activity_code,
+                                              LAG(REGEXP_SUBSTR (
+                                                     status.ISSUESTATENEW,
+                                                     '[#S]*\d{2}\w*'
+                                                  ))
+                                                 OVER (
+                                                    PARTITION BY tickets.oid
+                                                    ORDER BY status.MODIFIEDDATE
+                                                 )
+                                                 AS lag_activity_code,
+                                              status.MODIFIEDDATE AS TIMESTAMP,
+                                              LAG(status.MODIFIEDDATE)
+                                                 OVER (
+                                                    PARTITION BY tickets.oid
+                                                    ORDER BY status.MODIFIEDDATE
+                                                 )
+                                                 AS lag_timestamp
+                                       FROM      t_dev_milestones tickets
+                                              LEFT JOIN
+                                                 KASPERSK.ISSUESTATUSLOG status
+                                              ON tickets.oid = status.issue
+                                   ORDER BY   tickets.case_id,
+                                              status.MODIFIEDDATE)
+                          WHERE   (class_short = 'DEV'
+                                   AND   lag_activity_code
+                                      || '-'
+                                      || activity_code IN
+                                            ('#03-#04',               --demand
+                                             '#05-#06',
+                                             '#07-#08',
+                                             '#09-#10',
+                                             '#12-#13',
+                                             '#16-#17',              --release
+                                             '#18-#19',
+                                             '#21-#22',
+                                             '#24-#26',
+                                             '#27A-#28',
+                                             '#28-#29'))
+                                  OR (class_short = 'VIP'
+                                      AND   lag_activity_code
+                                         || '-'
+                                         || activity_code IN
+                                               ('#05-#06',            --demand
+                                                '#09-#10',
+                                                '#16-#17',           --release
+                                                '#18-#19',
+                                                '#21-#22',
+                                                '#24-#26',
+                                                '#27A-#28',
+                                                '#28-#29'))
+                                  OR (class_short = 'SDEV'
+                                      AND   lag_activity_code
+                                         || '-'
+                                         || activity_code IN
+                                               ('S09-S15',            --demand
+                                                'S16-S17',           --release
+                                                'S27A-S28'))
+                                  OR (class_short = 'RFC'
+                                      AND   lag_activity_code
+                                         || '-'
+                                         || activity_code IN
+                                               ('09-10',              --demand
+                                                        '17-18'))    --release
+                                                                 ))
+   GROUP BY   oid,
+              case_id,
+              class_short,
+              wait_phase
+   ORDER BY   oid,
+              case_id,
+              class_short,
+              wait_phase;
+
+COMMIT;
+
+
+
+/******************************************************************************/
+
+/* Update t_dev_milestones with wait times */
+
+/* Depends on t_dev_milestones and t_dev_wait_time*/
+
+/* Includes open and closed tickets */
+
+/******************************************************************************/
+ALTER TABLE t_dev_milestones
+ADD
+(
+demand_wait_days number,
+release_wait_days number
+);
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   demand_wait_days =
+            (SELECT   wait_time_day
+               FROM   t_dev_wait_time b
+              WHERE   a.oid = b.oid AND b.wait_phase = 'demand');
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   demand_wait_days = 0
+ WHERE   demand_wait_days IS NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   release_wait_days =
+            (SELECT   wait_time_day
+               FROM   t_dev_wait_time b
+              WHERE   a.oid = b.oid AND b.wait_phase = 'release');
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   release_wait_days = 0
+ WHERE   release_wait_days IS NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   start_close_net_days =
+            start_close_days - demand_wait_days - release_wait_days
+ WHERE   start_close_days IS NOT NULL
+         AND (demand_wait_days > 0 OR release_wait_days > 0);
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   demand_net_days = demand_days - demand_wait_days
+ WHERE   demand_days IS NOT NULL;
+
+COMMIT;
+
+UPDATE   t_dev_milestones a
+   SET   release_net_days = release_days - release_wait_days
+ WHERE   release_days IS NOT NULL;
+
+COMMIT;
+
 
 
 /* Query FTE for development phases */
-DROP TABLE t_dev_fte_phases;
-COMMIT;
+--DROP TABLE t_dev_fte_phases;
+--COMMIT;
 
-CREATE TABLE t_dev_fte_phases
-AS
-   SELECT   *
-     FROM   (  SELECT   a.*,
-                        wts_total_hours.user_worktimesheet,
-                        wts_total_hours.userorg_worktimesheet,
-                        wts_total_hours.created_worktimesheet,
-                        wts_total_hours.hours_worktimesheet,
-                        CASE
-                           WHEN a.release_start IS NOT NULL
-                                AND wts_total_hours.created_worktimesheet <
-                                      a.release_start
-                           THEN
-                              'Demand'
-                           WHEN a.release_start IS NOT NULL
-                                AND wts_total_hours.created_worktimesheet >=
-                                      a.release_start
-                           THEN
-                              'Release'
-                        END
-                           AS phase
-                 FROM      t_dev_milestones a
-                        LEFT JOIN
-                           (SELECT   a.issue,
-                                     b.name AS user_worktimesheet,
-                                     c.name AS userorg_worktimesheet,
-                                     a.created AS created_worktimesheet,
-                                     a.hours AS hours_worktimesheet
-                              FROM         KASPERSK.worktimesheet a
-                                        LEFT JOIN
-                                           KASPERSK.permissionpolicyuser b
-                                        ON a.owner = b.oid
-                                     LEFT JOIN
-                                        KASPERSK.organization c
-                                     ON b.defaultorganization = c.oid
-                             WHERE   timetype = 'WorkTime') wts_total_hours
-                        ON a.oid = wts_total_hours.issue
-             ORDER BY   CASE, created, created_worktimesheet)
-    WHERE   hours_worktimesheet IS NOT NULL;
+--CREATE TABLE t_dev_fte_phases
+--AS
+--   SELECT   *
+--     FROM   (  SELECT   a.*,
+--                        wts_total_hours.user_worktimesheet,
+--                        wts_total_hours.userorg_worktimesheet,
+--                        wts_total_hours.created_worktimesheet,
+--                        wts_total_hours.hours_worktimesheet,
+--                        CASE
+--                           WHEN a.release_start IS NOT NULL
+--                                AND wts_total_hours.created_worktimesheet <
+--                                      a.release_start
+--                           THEN
+--                              'Demand'
+--                           WHEN a.release_start IS NOT NULL
+--                                AND wts_total_hours.created_worktimesheet >=
+--                                      a.release_start
+--                           THEN
+--                              'Release'
+--                        END
+--                           AS phase
+--                 FROM      t_dev_milestones a
+--                        LEFT JOIN
+--                           (SELECT   a.issue,
+--                                     b.name AS user_worktimesheet,
+--                                     c.name AS userorg_worktimesheet,
+--                                     a.created AS created_worktimesheet,
+--                                     a.hours AS hours_worktimesheet
+--                              FROM         KASPERSK.worktimesheet a
+--                                        LEFT JOIN
+--                                           KASPERSK.permissionpolicyuser b
+--                                        ON a.owner = b.oid
+--                                     LEFT JOIN
+--                                        KASPERSK.organization c
+--                                     ON b.defaultorganization = c.oid
+--                             WHERE   timetype = 'WorkTime') wts_total_hours
+--                        ON a.oid = wts_total_hours.issue
+--             ORDER BY   CASE, created, created_worktimesheet)
+--    WHERE   hours_worktimesheet IS NOT NULL;
 
-COMMIT;
+--COMMIT;
--- SQL/gen_development_voting_time.sql
+++ SQL/gen_development_voting_time.sql
@@ -1,11 +1,115 @@
-/* Formatted on 2019. 09. 20. 13:04:37 (QP5 v5.115.810.9015) */
-  SELECT   tickets.oid,
-           tickets.CASE,
-           status.ISSUESTATENEW AS ACTIVITY,
-           status.MODIFIEDDATE AS TIMESTAMP,
-           lag (status.MODIFIEDDATE) over (partition by tickets.oid order by status.MODIFIEDDATE) as lag_timestamp
-    FROM      t_dev_milestones tickets
-           LEFT JOIN
-              KASPERSK.ISSUESTATUSLOG status
-           ON tickets.oid = status.issue
-ORDER BY   tickets.CASE, status.MODIFIEDDATE
+/* Compute pending days due to voting for development tickets */
+  SELECT                          /* Group wait times by phases for tickets */
+        oid,
+           case_id,
+           class_short,
+           wait_phase,
+           SUM (wait_time_day) AS wait_time_day
+    FROM   (SELECT /* Flag waiting times for demand and release phases separately */
+                  oid,
+                     case_id,
+                     class_short,
+                     activity,
+                     step,
+                     timestamp,
+                     lag_timestamp,
+                     wait_time_day,
+                     CASE
+                        WHEN (class_short = 'DEV'
+                              AND step IN
+                                       ('#03-#04',
+                                        '#05-#06',
+                                        '#07-#08',
+                                        '#09-#10',
+                                        '#12-#13'))
+                             OR (class_short = 'VIP'
+                                 AND step IN ('#05-#06', '#09-#10'))
+                             OR (class_short = 'SDEV' AND step IN ('S09-S15'))
+                             OR (class_short = 'RFC' AND step IN ('09-10'))
+                        THEN
+                           'demand'
+                        ELSE
+                           'release'
+                     END
+                        AS wait_phase
+              FROM   (SELECT /* Generate from-to phases for voting times for different ticket types */
+                            oid,
+                               case_id,
+                               class_short,
+                               activity,
+                               lag_activity_code || '-' || activity_code
+                                  AS step,
+                               timestamp,
+                               lag_timestamp,
+                               timestamp - lag_timestamp AS wait_time_day
+                        FROM   (  SELECT   tickets.oid,
+                                           tickets.case_id,
+                                           tickets.class_short,
+                                           status.ISSUESTATENEW AS ACTIVITY,
+                                           REGEXP_SUBSTR (status.ISSUESTATENEW,
+                                                          '[#S]*\d{2}\w*')
+                                              AS activity_code,
+                                           LAG(REGEXP_SUBSTR (
+                                                  status.ISSUESTATENEW,
+                                                  '[#S]*\d{2}\w*'
+                                               ))
+                                              OVER (PARTITION BY tickets.oid
+                                                    ORDER BY status.MODIFIEDDATE)
+                                              AS lag_activity_code,
+                                           status.MODIFIEDDATE AS TIMESTAMP,
+                                           LAG(status.MODIFIEDDATE)
+                                              OVER (PARTITION BY tickets.oid
+                                                    ORDER BY status.MODIFIEDDATE)
+                                              AS lag_timestamp
+                                    FROM      t_dev_milestones tickets
+                                           LEFT JOIN
+                                              KASPERSK.ISSUESTATUSLOG status
+                                           ON tickets.oid = status.issue
+                                ORDER BY   tickets.case_id, status.MODIFIEDDATE)
+                       WHERE   (class_short = 'DEV'
+                                AND lag_activity_code || '-' || activity_code IN
+                                         ('#03-#04',                  --demand
+                                          '#05-#06',
+                                          '#07-#08',
+                                          '#09-#10',
+                                          '#12-#13',
+                                          '#16-#17',                 --release
+                                          '#18-#19',
+                                          '#21-#22',
+                                          '#24-#26',
+                                          '#27A-#28',
+                                          '#28-#29'))
+                               OR (class_short = 'VIP'
+                                   AND   lag_activity_code
+                                      || '-'
+                                      || activity_code IN
+                                            ('#05-#06',               --demand
+                                             '#09-#10',
+                                             '#16-#17',              --release
+                                             '#18-#19',
+                                             '#21-#22',
+                                             '#24-#26',
+                                             '#27A-#28',
+                                             '#28-#29'))
+                               OR (class_short = 'SDEV'
+                                   AND   lag_activity_code
+                                      || '-'
+                                      || activity_code IN
+                                            ('S09-S15',               --demand
+                                                       'S16-S17',    --release
+                                                                 'S27A-S28'))
+                               OR (class_short = 'RFC'
+                                   AND   lag_activity_code
+                                      || '-'
+                                      || activity_code IN
+                                            ('09-10',                 --demand
+                                                     '17-18'))       --release
+                                                              ))
+GROUP BY   oid,
+           case_id,
+           class_short,
+           wait_phase
+ORDER BY   oid,
+           case_id,
+           class_short,
+           wait_phase;
